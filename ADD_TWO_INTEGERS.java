a1edd3220a8700b976d9547a233632a4cf5fe114
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2020-05-15 10:45:41 +0530

    Create ADD_TWO_INTEGERS.java

---
+++ ADD_TWO_INTEGERS.java
@@ -0,0 +1,14 @@
+package com.codewithint;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int first = 10;
+        int second = 20;
+
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
