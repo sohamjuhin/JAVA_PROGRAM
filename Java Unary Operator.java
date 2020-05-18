058a3d33c54ee516bb55f44a1397498a872f95ad
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2020-05-18 20:44:56 +0530

    Create Java Unary Operator.java

---
+++ Java Unary Operator.java
@@ -0,0 +1,16 @@
+package com.codewithint;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a=10;
+       int b=-10;
+         boolean c=true;
+        boolean d=false;
+        System.out.println(~a);    //-11(minusoftotalpositivevaluewhichstartsfrom0)
+        System.out.println(~b);    //9(positiveoftotalminus,positivestartsfrom0)
+        System.out.println(!c);    //false(oppositeofbooleanvalue)
+        System.out.println(!d);     //true 11. }}
+    }
+}
