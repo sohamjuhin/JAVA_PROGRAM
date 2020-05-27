524ac388f67ca969ff8cabd28837fbfafcf7432f
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2020-05-27 11:44:37 +0530

    Create AND_OPERATOR.java

---
+++ AND_OPERATOR.java
@@ -0,0 +1,13 @@
+package com.codewithint;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a=0;
+        int b=5;
+        int c=2;
+        System.out.println(a<b&&a<c); //false&&true=false 7. System.out.println(a<b&a<c);//false&true=false
+
+    }
+}
